fc03c7fe71d54efbc0d6b35b6bfa91a7486c99fd
Alexander Litovchenko <agutierrezlitovchenko@frba.utn.edu.ar>
2026-04-19 19:57:00 -0300

    agrego modelado de usuario

---
+++ src/models/usuario.model.js
@@ -0,0 +1,13 @@
+import { usuarioSchema } from '../schemas/usuario.schema.js'
+
+export class Usuario {
+    #id
+    #nombreUsuario
+    #password
+
+    constructor({ id, nombreUsuario, password }){
+        this.#id = id
+        this.#nombreUsuario = nombreUsuario
+        this.#password = password
+    }
+}
--- src/schemas/usuario.schema.js
+++ src/schemas/usuario.schema.js
@@ -1,7 +1,6 @@
 import { z } from 'zod'
 
 export const usuarioSchema = z.object({
-    id: z.string(),
     nombreUsuario: z.string(),
     password: z.string()
 })
