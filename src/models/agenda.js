198085a0e95067aa1a3ebf5ad8a85656c66bc8c7
ahuarca <ahuarca@frba.utn.edu.ar>
2026-04-23 17:50:17 -0300

    agrego comentarios en agenda

--- src/models/agenda.js
+++ src/models/agenda.js
@@ -1,28 +1,39 @@
-import { FactoryNotificacion } from './notificacion.factory.js';
 import { Notificacion } from "./notificacion.model.js";
 import { Especialidad } from './especialidad.js';
 import { Practica } from './practica.js';
 export class Agenda {
   
    static generarTurnosPara(especialidad, medico){
-
-        
-       
-        /*retorna una lista de turnos -> Turno[]*/
-        // return turnos;
+        // agarro al medico y compruebo que tiene es especialidad
+        const tieneEspecialidad = medico.especialidades.some(especialidadMedico => especialidadMedico.id === especialidad.id);
+     
+        if (tieneEspecialidad){
+           /*aca tendria que hacerse una cosulta a la base de datos
+            para traer los turnos del medico que correspan a dicha especialidad */   
+        }else{
+            throw new Error(`El médico ${medico.nombre} no tiene la especialidad ${especialidad.nombre}`);
+        }
     }
+   
 
    static generarTurnosPara(practica, medico){
 
-      
-         /*retorna una lista de turnos -> Turno[]*/
-        // return turnos;
+       // agarro al medico y compruebo que tiene esa practica
+        const tienePractica = medico.practicas.some(practicaMedico => practicaMedico.id === practica.id);
+     
+        if (tienePractica){
+           /*aca tendria que hacerse una cosulta a la base de datos
+            para traer los turnos del medico que correspan a dicha practica */   
+        }else{
+            throw new Error(`El médico ${medico.nombre} no tiene la practica ${practica.nombre}`);
+        }
     }
 
     static refrescarTurnosSegunDisponibilidadDe(medico){
 
-        /*retorna una lista de turnos -> Turno[]*/
-        // return turnos;
+        /*Aca tendrian que traerse todos los turnos del medico, que esten con estado reservado y 
+        con fecha posterior a la actual, de la base de datos y modificarlos segun si la nueva disponilidad del medico*/
+       
     }
 
 
--- src/schemas/disponibilidadHoraria.schema.js
+++ src/schemas/disponibilidadHoraria.schema.js
@@ -2,7 +2,7 @@ import {z} from 'zod';
 import {diaSemanaSchema, horaSchema} from './horaDia.schema.js';
 export const disponibilidadHorariaSchema = z.object({
     diaSemana: diaSemanaSchema,
-    horaDesde: horaSchema,
+    horaDesde: horaSchema, 
     horaHasta: horaSchema,
 }).refine(
     (data)=> data.horaDesde<data.horaHasta, { message: 'HoraDesde debe ser menor que HoraHasta'}
