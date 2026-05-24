3e4f0a66531c9a067d07e2232b45f0981e222e46
ahuarca <ahuarca@frba.utn.edu.ar>
2026-05-23 21:00:47 -0300

    agrego conexion bd

--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
 import app from './app.js'
+import dotenv from 'dotenv'
 import { cargarDatosDePruebaEnMemoria } from './repositories/datosPrueba.enMemoria.js'
+import { MongoDBClient } from './config/database.js'
 
-const PORT = 3000
+dotenv.config()
+
+const PORT = process.env.PUERTO || 3000
 
 cargarDatosDePruebaEnMemoria()
 
+const start = async () => {
+    try{
+        await MongoDBClient.connect()
+        // server.port = PORT
+        // server.launch()
+    }
+    catch(error){
+        console.error(error);
+    }
+}
 app.listen(PORT, () => {
     console.log('--- Sistema Sweet Medical ---');
     console.log(`Servidor escuchando en: http://localhost:${PORT}`);
