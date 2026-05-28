e0b56453fdac9c80f835ff9b16b2f7962abe2ef5
Alexander Litovchenko <agutierrezlitovchenko@frba.utn.edu.ar>
2026-05-28 11:45:24 -0300

    agrego rule para detectar console.log

--- eslint.config.js
+++ eslint.config.js
@@ -15,7 +15,7 @@ export default [
     },
     rules: {
       'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      'no-console': 'off',
+      'no-console': 'warn',
       'no-undef': 'error',
     },
   },
