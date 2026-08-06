cae59f68bc0ff6572d5a62468dd98cdca7a4ffec
ahuarca <ahuarca@frba.utn.edu.ar>
2026-08-06 15:03:03 -0300

    agrego models que se me borraron

---
+++ server/src/models/cambioEstadoTurno.js
@@ -0,0 +1,15 @@
+export class CambioEstadoTurno {
+  fechaHoraIngreso
+  estado
+  turno
+  usuario
+  motivo
+
+  constructor(fechaHoraIngreso, estado, turno, usuario, motivo) {
+    this.fechaHoraIngreso = fechaHoraIngreso
+    this.estado = estado
+    this.turno = turno
+    this.usuario = usuario
+    this.motivo = motivo
+  }
+}
